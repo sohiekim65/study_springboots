f445019137b6295e567943c426da638e0c29466d
sohiekim65 <sohiekim65@gmail.com>
2023-01-05 12:12:46 +0900

    first commit

---
+++ src/main/java/com/study/study_springboots/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.study.study_springboots.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// 서블릿에서 했었던 urlMapping과 componentScan을 @Controller, @RequestMapping
+@Controller // 이 클래스에다가 Controller라고 캡을 씌워서 문지기가 기억하도록
+public class HomeController {
+    @RequestMapping(value = {"", "/", "/main"}) // url이 이 3개면 main.jsp로 들어오도록(url여러개 가능) 
+    public String main(){
+        int i = 0;
+        return "WEB-INF/views/main.jsp";
+    }
+    @RequestMapping(value = "/home") 
+    public void home(){
+        int i = 0;
+    }
+    @RequestMapping(value = "/homejsp") // http://localhost:8080/homejsp
+    public String homejsp(){
+        int i = 0;
+        return "WEB-INF/views/home.jsp";  // /homejsp를 호출하면 home.jsp 화면에 보이도록
+    }
+    @RequestMapping(value = "/homehtml") // http://localhost:8080/homehtml
+    public String homehtml(){
+        int i = 0;
+        return "WEB-INF/views/home.html"; // html은 spring에서 해석하지 않고 jsp만 해석한다(jsp를 해석할 수 있는 건 jasper 라이브러리 덕분).
+    }
+}
