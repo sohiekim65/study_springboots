550531095467e97ad9243a6e8a235cfe428be182
sohiekim65 <sohiekim65@gmail.com>
2023-01-30 22:31:38 +0900

    add some files

--- src/main/java/com/study/study_springboots/utils/Paginations.java
+++ src/main/java/com/study/study_springboots/utils/Paginations.java
@@ -1,20 +1,58 @@
 package com.study.study_springboots.utils;
 
 public class Paginations {
-    private int pageScale = 10; // 한 페이지 당 게시물(데이터) 개수
-    private int currentPage;    // 클릭한 현재 페이지 번호 currentPage는 그때 그때 다름.
-    private int totalPage;  // 전체 페이지 개수(75개니까 페이지로는 8페이지(데이터 10개씩이니까))
-    private int totalCount; // 전체 리스트(게시물) 개수
-    private int pageBegin;  // 페이지 내에서 리스트 시작 번호(123에서 1)
-    private int pageEnd;    // 페이지 내에서 리스트 끝나는 번호(123에서 3)
+    private int pageScale = 10; // 페이지당(데이터) 게시물 수
+	private int blockScale = 5; // 블록당 페이지수
+	private int currentPage; // 현재 페이지 번호
+	private int previousPage; // 이전 페이지
+	private int nextPage; // 다음 페이지
+	private int totalPage; // 전체 페이지갯수 (75개니까 페이지로는 8페이지(데이터 10개씩이니까))
+	private int currentBlock; // 현재 페이지 블록 번호
+	private int totalBlock; // 전체 페이지 블록 갯수
+	private int pageBegin; // 페이지 내에서의 레코드 시작 번호(123에서 1)
+	private int pageEnd; // 페이지 내에서의 레코드 마지막 번호(123에서 3)
+	private int blockStart; // 페이지 블록 내에서의 시작 페이지 번호
+	private int blockEnd; // 페이지 블록 내에서의 마지막 페이지 번호
+	private int totalCount; // 페이지 블록 내에서의 마지막 페이지 번호
 
+    
     public Paginations(int totalCount, int currentPage) {
         // 넘어오는 값 : 전체 개수(totalCount), 현재 페이지 개수(currentPage)
         this.totalCount = totalCount;
         this.currentPage = currentPage;
         this.setTotalPage(totalCount); // totalPage 세팅이 됨
         this.setPageRange();
+        this.setTotalBlock();
+        this.setBlockRange();
     }
+
+    // 전체 페이지 블록 갯수 계산
+	public void setTotalBlock() {
+		totalBlock = (int) Math.ceil(totalPage / blockScale);
+        // ceil 반올림
+	}
+
+    // 현재 페이지가 몇번째 페이지에 속하는지 계산
+	public void setBlockRange() {
+		// 현재 페이지가 몇번째 페이지 블록에 속하는지 계산
+		currentBlock = (int) Math.ceil((currentPage - 1) / blockScale) + 1;
+		blockStart = (currentBlock - 1) * blockScale + 1; // 시작번호
+		blockEnd = blockStart + blockScale - 1; // 끝번호
+		if (blockEnd > totalPage) { // 마지막 페이지가 범위를 초과할 경우
+			blockEnd = totalPage;
+		}
+
+		// 현재 블록이 1이면 이전 페이지를 1로 설정
+		previousPage = currentBlock == 1 ? 1 : (currentBlock - 1) * blockScale;
+
+		// 현재 블록이 마지막 블록이면 다음 페이지를 마지막 페이지 번호로 설정
+		nextPage = currentBlock > totalBlock ? (currentBlock * blockScale) : (currentBlock * blockScale) + 1;
+
+		// 마지막 페이지가 범위를 넘지 않도록 처리
+		if (nextPage >= totalPage) {
+			nextPage = totalPage;
+		}
+	}
 
     // 페이지네이션의 << 123 >> << 456 >> 이렇게 표시하는
     public void setPageRange() {
@@ -23,9 +61,10 @@ public class Paginations {
     }
 
     public void setTotalPage(int totalCount) {
-        this.totalPage = (int) Math.ceil(totalCount / this.pageScale); 
+        this.totalPage = (int) Math.ceil(totalCount * 1.0 / this.pageScale); 
         // (totalPage = 75 / 10인데 소수점으로 나와서 Math function 사용)
     }
+
 
     // getter setter
     public int getPageScale() {
@@ -70,6 +109,62 @@ public class Paginations {
 
     public void setPageEnd(int pageEnd) {
         this.pageEnd = pageEnd;
+    }
+
+    public int getBlockScale() {
+        return blockScale;
+    }
+
+    public void setBlockScale(int blockScale) {
+        this.blockScale = blockScale;
+    }
+
+    public int getPreviousPage() {
+        return previousPage;
+    }
+
+    public void setPreviousPage(int previousPage) {
+        this.previousPage = previousPage;
+    }
+
+    public int getNextPage() {
+        return nextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        this.nextPage = nextPage;
+    }
+
+    public int getCurrentBlock() {
+        return currentBlock;
+    }
+
+    public void setCurrentBlock(int currentBlock) {
+        this.currentBlock = currentBlock;
+    }
+
+    public int getTotalBlock() {
+        return totalBlock;
+    }
+
+    public void setTotalBlock(int totalBlock) {
+        this.totalBlock = totalBlock;
+    }
+
+    public int getBlockStart() {
+        return blockStart;
+    }
+
+    public void setBlockStart(int blockStart) {
+        this.blockStart = blockStart;
+    }
+
+    public int getBlockEnd() {
+        return blockEnd;
+    }
+
+    public void setBlockEnd(int blockEnd) {
+        this.blockEnd = blockEnd;
     }
 
     
