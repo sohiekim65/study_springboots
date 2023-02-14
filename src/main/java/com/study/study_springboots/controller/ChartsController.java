e073b1454cb8844d3524e9b92ceb5b11a9402fd0
sohiekim65 <sohiekim65@gmail.com>
2023-02-14 11:25:13 +0900

    add chart files

---
+++ src/main/java/com/study/study_springboots/controller/ChartsController.java
@@ -0,0 +1,15 @@
+package com.study.study_springboots.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ChartsController {
+    @RequestMapping(value = "/charts/google_charts_scatter", method = RequestMethod.GET)
+    public ModelAndView edit(ModelAndView modelAndView){
+        modelAndView.setViewName("charts/google_charts_scatter");
+        return modelAndView;
+    }
+}
