9ae50324d1bec3b540cdb1ba5394ca210bca7345
sohiekim65 <sohiekim65@gmail.com>
2023-02-14 10:46:15 +0900

    add some files

--- src/main/java/com/study/study_springboots/controller/RestfulController.java
+++ src/main/java/com/study/study_springboots/controller/RestfulController.java
@@ -2,15 +2,20 @@ package com.study.study_springboots.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.study.study_springboots.service.CommonCodeOurService;
+
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 // 기존 controller의 완성된 html을 전달하는 것이 아닌 데이터만 전달하는 controller
 @RestController
+@CrossOrigin
 public class RestfulController {
 
     @RequestMapping(value = "/api/v1/helloworld", method = RequestMethod.GET)
@@ -26,5 +31,20 @@ public class RestfulController {
         result.putAll(params);
         result.put("id", "sohiekim");
         return result;
+    }
+
+    @Autowired
+    CommonCodeOurService commonCodeOurService;
+
+    // currentPage : 1
+    @RequestMapping(value = "/api/v1/requestParamsWithDB", method = RequestMethod.POST)
+    public Map requestParamsWithDB(@RequestParam Map<String, Object> params) {
+        params.put("currentPage", Integer.parseInt((String)params.get("currentPage")));
+        params.put("pageScale", 10); 
+
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap = (Map<String, Object>) commonCodeOurService.getListWithPagination(params);
+
+        return resultMap;
     }
 }
